385f0f8e795b7b3c263caafb1e5305cb462e7226
nmparfait <mbamu.developer@gmail.com>
2021-01-25 21:47:05 +0000

    new visit added to the dictionary

--- main.py
+++ main.py
@@ -3,28 +3,42 @@ travel_log = {
   {
     "cities_visited": ["Kinshasa", "Abidjan", "Dakar", "Johannesbourg", "Douala", "Zanzibar"], 
   "citiesTo_visit": ["Brazza", "Kigali", "Cap Town"],
-  "visit": 17
+  "time": 17
   },
 
   "Europe": 
   {
     "cities_visited": ["Lille", "Paris", "Rome", "Bruxelles", "Berlin"], 
   "citiesTo_visit": ["Lyon", "Viennes", "Luxembourg", "Liege"] },
-  "visit": 15,
+  "time": 15,
 
   "Asie": 
   {
     "cities_visited": ["Tokyo", "Seoul", "Shanghai"],
   "citiesTo_visit": ["Dubai", "New Delhi", "Bombay"],
-  "visit": 12  
+  "time": 12  
   },
   "Oceanie":
   {
     "cities_visited": ["Wellington", "Sidney","Canberra"],
     "citiesTo_visit": ["Adelaide", "Suva", "Melbourne", "Papeete"],
-    "visit": 7
+    "time": 7
   }
 
 }
+
+print(travel_log)
+
+# fuction to add more countries into the dictionary
+
+def add_new_visit(cities_visited, citiesTo_visit, time):
+  new_travel = {}
+  new_travel["city"] = cities_visited
+  new_travel["toVisit"] = citiesTo_visit
+  new_travel["number"] = time
+  travel_log.append(new_travel)
+
+
+add_new_visit(["Moscou","Roma","London"], ["Venise", "Strasbourg"], 6)
 
 print(travel_log)
