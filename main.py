e0b1871f916d45f7f3a986823e91b8e2ef681ed0
nmparfait <mbamu.developer@gmail.com>
2021-01-25 21:31:01 +0000

    dictionary created

---
+++ main.py
@@ -0,0 +1,30 @@
+travel_log = {
+  "Africa": 
+  {
+    "cities_visited": ["Kinshasa", "Abidjan", "Dakar", "Johannesbourg", "Douala", "Zanzibar"], 
+  "citiesTo_visit": ["Brazza", "Kigali", "Cap Town"],
+  "visit": 17
+  },
+
+  "Europe": 
+  {
+    "cities_visited": ["Lille", "Paris", "Rome", "Bruxelles", "Berlin"], 
+  "citiesTo_visit": ["Lyon", "Viennes", "Luxembourg", "Liege"] },
+  "visit": 15,
+
+  "Asie": 
+  {
+    "cities_visited": ["Tokyo", "Seoul", "Shanghai"],
+  "citiesTo_visit": ["Dubai", "New Delhi", "Bombay"],
+  "visit": 12  
+  },
+  "Oceanie":
+  {
+    "cities_visited": ["Wellington", "Sidney","Canberra"],
+    "citiesTo_visit": ["Adelaide", "Suva", "Melbourne", "Papeete"],
+    "visit": 7
+  }
+
+}
+
+print(travel_log)
